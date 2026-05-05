b8d521c4d7bbb7c2c377689accbcaaa70f1bd6ea
Matias-Arisi-Guliasdad <49960010@est.ort.edu.ar>
2026-05-05 14:27:21 -0300

    Initial commit

---
+++ Assets/Scripts/Colision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    CubeScripts cubeData;
+ 
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Hubo colision");
+        cubeData = col.gameObject.GetComponent<CubeScripts>();
+        Debug.Log("Nombre del otro objeto:"+ cubeData.ObjectName);
+
+    }
+}
---
+++ Assets/Scripts/CubeScripts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScripts : MonoBehaviour
+{
+    public string ObjectName;
+    public int value;
+
+}
